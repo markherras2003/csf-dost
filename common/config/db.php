fbaeb463fb78e4343e2377748dd4bc7713fc056d
markherras2003 <larrymark2003@gmail.com>
2019-10-28 09:33:14 +0800

    Update db.php

--- common/config/db.php
+++ common/config/db.php
@@ -4,17 +4,17 @@ return [
     'db'=>
     [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=192.168.1.96;dbname=csf',
-        'username' => 'csfuser',
-        'password' => 'D057R3g10n9!@#',
+        'dsn' => 'mysql:host=localhost;dbname=csf',
+        'username' => 'root',
+        'password' => 'root',
         'charset' => 'utf8',
         'tablePrefix' => 'tbl_',
     ],
     'csfdb'=>[
         'class' => 'yii\db\Connection',  
-        'dsn' => 'mysql:host=192.168.1.96;dbname=csf-main',
-        'username' => 'csfuser',
-        'password' => 'D057R3g10n9!@#',
+        'dsn' => 'mysql:host=localhost;dbname=csf-main',
+        'username' => 'root',
+        'password' => 'root',
         'charset' => 'utf8',
         'tablePrefix' => 'tbl_',
     ],
