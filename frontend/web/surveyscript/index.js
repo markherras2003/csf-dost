c1fc11ae96c52cfb247f4de21da9601279f2bca1
markherras2003 <larrymark2003@gmail.com>
2019-11-05 16:10:14 +0800

    up

--- frontend/web/surveyscript/index.js
+++ frontend/web/surveyscript/index.js
@@ -722,3 +722,28 @@ footer.appendChild(cancelBtn);
 
 
 });
+
+
+
+
+$( document ).on( "click", ".openmode", function () {
+
+Swal.fire({
+   title: 'Are you sure?',
+   text: "You won't be able to revert this!",
+   icon: 'warning',
+   showCancelButton: true,
+   confirmButtonColor: '#3085d6',
+   cancelButtonColor: '#d33',
+   confirmButtonText: 'Yes, delete it!'
+ }).then((result) => {
+   if (result.value) {
+     Swal.fire(
+       'Deleted!',
+       'Your file has been deleted.',
+       'success'
+     )
+   }
+ });
+
+});
