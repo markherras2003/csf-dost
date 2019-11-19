61ba6e52f6f58beb22bd1a31e5363ffec6775d01
markherras2003 <larrymark2003@gmail.com>
2019-11-19 09:28:35 +0800

    dd

--- frontend/web/surveyscript/db.php
+++ frontend/web/surveyscript/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("192.168.1.96","D057R3g10n9!@#", "D057R3g10n9!@#","csf-main") or die ("could not connect database");
+ $con = mysqli_connect("192.168.1.96","csfuser", "D057R3g10n9!@#","csf-main") or die ("could not connect database");
 ?>
