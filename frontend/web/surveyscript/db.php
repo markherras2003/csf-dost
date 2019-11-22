27a76039bb4bfb14f57e29b2042cb9e35d7f98fe
markherras2003 <larrymark2003@gmail.com>
2019-11-22 15:48:20 +0800

    Update db.php

--- frontend/web/surveyscript/db.php
+++ frontend/web/surveyscript/db.php
@@ -1,4 +1,5 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","root", "root","csf") or die ("could not connect database");
+ //$con = mysqli_connect("localhost","root", "root","csf") or die ("could not connect database");
+ $con = mysqli_connect("192.168.1.96","csfuser", "D057R3g10n9!@#","csf") or die ("could not connect database");
 ?>
